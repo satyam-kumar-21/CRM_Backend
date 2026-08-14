d6cf2426555c09a3432dbb76cdd41ae1e4d03591
Satyam kumar <satyamkumar2716@gmail.com>
2026-08-15 00:34:34 +0530

    updated

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,8 @@ import { connectDB } from './config/db';
 import { createServer } from 'http';
 import { configureSocket } from './realtime/socket';
 
-const PORT = process.env.PORT || 5000;
+const PORT: number = Number(process.env.PORT) || 5000;
+// const HOST = '0.0.0.0';
 
 const startServer = async () => {
   await connectDB();
